8a9918cd0ead40cf3416177fe4469d2f03fda580
zorianach <zoryanamirchuk@gmail.com>
2024-04-02 21:06:37 +0300

    fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,8 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
 import HomePage from '../pages/Home/Home';
 import WelcomePage from '../pages/Welcome/Welcome';
-<<<<<<< Updated upstream
-import {PrivateRoute} from "../guards/PrivateRoute";
-import {PublicRoute} from "../guards/PublicRoute";
-=======
 import PrivateRoute from '../guards/PrivateRoute';
 import PublicRoute from '../guards/PublicRoute';
->>>>>>> Stashed changes
 import SignUpPage from '../pages/Signup/Signup';
 import SignInPage from '../pages/Singin/Singin';
 
