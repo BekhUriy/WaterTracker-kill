74d381755b5c924fa4dde43fbef14e78064ec351
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-02 13:13:50 +0300

    fix2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { UserAuth } from './UserAuth/UserAuth';
 import { UserButton } from './userButton/userButton';
 import { Block, HeaderStyled } from './header-styled';
 import { useSelector } from 'react-redux';
-//import { SiteLogo } from "./siteLogo/siteLogo";
+import { SiteLogo } from './siteLogo/siteLogo';
 
 const Header = () => {
   const isLogin = useSelector((state) => state.auth.isLogin);
--- src/components/header/UserAuth/UserAuth.jsx
+++ src/components/header/UserAuth/UserAuth.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { UserLogoSvg } from "../userLogoSvg/userLogoSvg";
-import { SignInStyled } from "./UserAuth-style";
+import React from 'react';
+import { UserLogoSvg } from '../userLogoSvg/userLogoSvg';
+import { SignInStyled } from './UserAuth-style';
 
 export const UserAuth = () => {
-    return (
-        <SignInStyled type="button" media>
-            Sign In <UserLogoSvg/>
-        </SignInStyled>
-    )
-}
+  return (
+    <SignInStyled type="button" media>
+      Sign In <UserLogoSvg />
+    </SignInStyled>
+  );
+};
