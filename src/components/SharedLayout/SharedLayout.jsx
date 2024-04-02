69ca76ffbcda449f300ec437e0136c684f2a4d6f
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-03 00:08:59 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { LayoutBox } from './SharedLayout.styled';
-import Header from './Header/Header';
+import Header from '../Header/Header';
 
 const SharedLayout = () => {
   return (
