853bff1c20413972dff990c9baa4034fb7838981
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-02 13:05:14 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,21 @@
-import React from "react";
-import { UserAuth } from "./UserAuth/UserAuth";
-import { UserButton } from "./userButton/userButton";
-import { Block, HeaderStyled } from "./header-styled";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { UserAuth } from './UserAuth/UserAuth';
+import { UserButton } from './userButton/userButton';
+import { Block, HeaderStyled } from './header-styled';
+import { useSelector } from 'react-redux';
 //import { SiteLogo } from "./siteLogo/siteLogo";
 
 const Header = () => {
-    const isLogin = useSelector(state => state.auth.isLogin)
+  const isLogin = useSelector((state) => state.auth.isLogin);
 
+  return (
+    <HeaderStyled media>
+      <div>
+        <SiteLogo />
+      </div>
+      <Block>{!isLogin ? <UserAuth /> : <UserButton />}</Block>
+    </HeaderStyled>
+  );
+};
 
-    return (
-        <HeaderStyled media>
-            <div>
-                <SiteLogo />
-            </div>
-            <Block>
-            {!isLogin ?
-                (<UserAuth /> ):
-                (<UserButton />)}
-            </Block>
-        </HeaderStyled>
-    )
-}
-
-export default Header
+export default Header;
