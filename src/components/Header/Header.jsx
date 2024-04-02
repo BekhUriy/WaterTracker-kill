9c609cee20fb58b99e918d74639a5cc347e3db33
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-02 17:12:14 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UserAuth } from '../Header/UserAuth/UserAuth';
+import { UserAuth } from '../header/UserAuth/UserAuth';
 import { UserButton } from './userButton/userButton';
 import { Block, HeaderStyled } from './header-styled';
 import { useSelector } from 'react-redux';
