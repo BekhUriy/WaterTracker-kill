15ece152277279aabca314307cd0b41f9abdc921
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-03 03:04:54 +0300

    fix

--- src/API/ApiAuth.js
+++ src/API/ApiAuth.js
@@ -5,25 +5,7 @@ export const authApi = axios.create({
 });
 
 export const signUpApi = async (body) => {
-  try {
-    const { data } = await authApi.post('auth/signup', body);
-    return data;
-  } catch (error) {
-    console.error('Error in signUpApi:', error);
-    throw error;
-  }
-};
-
-export const loginApi = async (body) => {
-  try {
-    const { data } = await authApi.post('auth/login', body);
-    return data;
-  } catch (error) {
-    console.error('Error in loginApi:', error);
-    throw error;
-  }
-};
-
+@@ -27,7 +27,7 @@ export const loginApi = async (body) => {
 export const refreshApi = async (token) => {
   try {
     const { data } = await authApi.post(
@@ -41,7 +23,6 @@ export const refreshApi = async (token) => {
     throw error;
   }
 };
-
 export const logoutApi = async (token) => {
   try {
     const { data } = await authApi.post(
@@ -59,40 +40,3 @@ export const logoutApi = async (token) => {
     throw error;
   }
 };
-
-// import axios from "axios"
-// export const authApi = axios.create({
-//   baseURL: 'https://practices-api.vercel.app/',
-// })
-
-// export const signUpApi = async (body) => {
-//   const { data } = await authApi.post('auth/signup', body)
-//   return data
-// }
-
-// export const loginApi = async (body) => {
-//   const { data } = await authApi.post('auth/login', body)
-//   return data
-// }
-
-// export const refreshApi = async (token) => {
-//   const { data } = await authApi('auth/refresh', {
-//     headers: {
-//       Authorization: Bearer ${token},
-//     },
-//   })
-//   return data
-// }
-
-// export const logoutApi = async token => {
-//   const { data } = await authApi.post(
-//     'auth/logout',
-//     {},
-//     {
-//       headers: {
-//         Authorization: Bearer ${token},
-//       },
-//     }
-//   );
-//   return data;
-// };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,74 +36,9 @@ function App() {
             <PublicRoute component={<SignInPage />} redirectTo="/home" />
           }
         />
-
         <Route path="*" element={<WelcomePage />} />
       </Route>
     </Routes>
   );
 }
 export default App;
-
-// import { Route, Routes } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useEffect } from 'react';
-// import { GlobalStyles } from '../GlobalStyles';
-// import SharedLayout from './SharedLayout/SharedLayout';
-// import HomePage from '../pages/Home/Home';
-// import WelcomePage from '../pages/Welcome/Welcome';
-// import { PrivateRoute } from '../guards/PrivateRoute';
-// import { PublicRoute } from '../guards/PublicRoute';
-// import SignUpPage from '../pages/Signup/Signup';
-// import SignInPage from '../pages/Singin/Singin';
-// import { profileSelector } from '../redux/auth/selectors';
-// import { refreshThunk } from '../redux/auth/thunk';
-// // const test = import.meta.env.VITE_API_TEST;
-
-// function App() {
-//   // console.log(test);
-
-//   // const dispatch = useDispatch();
-//   const profile = useSelector(profileSelector);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     !profile && dispatch(refreshThunk());
-//   }, [dispatch, profile]);
-
-//   return (
-//     <>
-//       <GlobalStyles />
-//       <Routes>
-//         <Route path="/" element={<SharedLayout />}>
-//           {/* <Route index element={<WelcomePage />} /> */}
-//           <Route
-//             index
-//             element={
-//               <PublicRoute redirectTo="/home" component={<WelcomePage />} />
-//             }
-//           />
-//           <Route
-//             path="home"
-//             element={<PrivateRoute redirectTo={'/'} component={<HomePage />} />}
-//             // {<PublicRoute redirectTo={'/home'} component={<HomePage />}/>}
-//           />
-//           <Route
-//             path="signup"
-//             element={
-//               <PublicRoute component={<SignUpPage />} redirectTo="/home" />
-//             }
-//           />
-//           <Route
-//             path="signin"
-//             element={
-//               <PublicRoute component={<SignInPage />} redirectTo="/home" />
-//             }
-//           />
-
-//           <Route path="*" element={<WelcomePage />} />
-//         </Route>
-//       </Routes>
-//     </>
-//   );
-// }
-// export default App;
