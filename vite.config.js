5cffef39e2f0b14653a044d1ac45c78e1c0472b7
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-03 04:10:57 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@src': './src',
-      '@components': './src/components',
-      '@pages': './src/pages',
-      '@assets': './src/assets',
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
     },
   },
   base: 'https://bekhuriy.github.io/WaterTracker/',
