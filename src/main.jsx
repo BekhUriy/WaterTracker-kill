414f0dfdb01ab92ade61d5074f92ef02c537f893
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-04-03 03:28:29 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store';
@@ -10,15 +9,17 @@ import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter basename="/WaterTracker">
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <BrowserRouter basename="/WaterTracker">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
-      </PersistGate>
-    </Provider>
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-{/* <BrowserRouter basename="/">
+{
+  /* <BrowserRouter basename="/">
             <App />
-          </BrowserRouter> */}
+          </BrowserRouter> */
+}
